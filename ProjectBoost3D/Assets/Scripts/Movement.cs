3aab629343cae67420ed5cc0a1ab11b8e87400b2
Kulac12 <kulacedanur460@gmail.com>
2022-03-12 01:17:27 +0300

    İtme hızı için dışarıdan değişken alma
    itme hızını dışarıdan tanımlama ve değiştime olanağı

--- ProjectBoost3D/Assets/Scripts/Movement.cs
+++ ProjectBoost3D/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] float mainThrust = 100f; //itme gücü speed gibi
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +27,7 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up);   
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime );   
             Debug.Log("Press Space - Thrusting");
         }
         
